d9a2daa732757cd01c64aec96b28d4ed58d5be96
larryg <larryg@klassitech.co.uk>
2025-09-17 16:20:55 +0100

    chore(): updating code

--- step_definitions/search-steps.js
+++ step_definitions/search-steps.js
@@ -4,8 +4,23 @@ const pageObjectMap = require('../shared-objects/pageObjectData');
 let activePageObject;
 
 Before((scenario) => {
-  activePageObject = getActivePageObject(scenario, pageObjectMap);
+  console.log('Scenario tags:', scenario.pickle.tags.map(tag => tag.name));
+  console.log('Available page object tags:', Object.keys(pageObjectMap));
+  console.log('Page object map values:', Object.values(pageObjectMap).map(po => po ? typeof po : 'null'));
+  
+  // Filter out null page objects
+  const validPageObjectMap = {};
+  for (const [tag, pageObject] of Object.entries(pageObjectMap)) {
+    if (pageObject !== null) {
+      validPageObjectMap[tag] = pageObject;
+    }
+  }
+  
+  console.log('Valid page object tags:', Object.keys(validPageObjectMap));
+  
+  activePageObject = getActivePageObject(validPageObjectMap);
   console.log('activePageObject ========================== 1 :', activePageObject);
+  console.log('activePageObject methods:', Object.getOwnPropertyNames(activePageObject).filter(name => typeof activePageObject[name] === 'function'));
 });
 
 Given(/^The user arrives on the duckduckgo search page$/, async () => {
@@ -13,7 +28,7 @@ Given(/^The user arrives on the duckduckgo search page$/, async () => {
 });
 
 When(/^they input (.*)$/, async (searchWord) => {
-  if (!activePageObject || !activePageObject.performWebSearch(searchWord)) {
+  if (!activePageObject || !activePageObject.performWebSearch) {
     throw new Error('The active page object does not have a performWebSearch method!');
   }
   await activePageObject.performWebSearch(searchWord);
